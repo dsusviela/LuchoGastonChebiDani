d687ff0d7152870ba50acf8682cbf019a3020e83
dsusviela <yothebest009@gmail.com>
2017-03-17 00:33:57 -0300

    importe alguna cosa del dropbox

--- include/clase.h
+++ include/clase.h
@@ -1,3 +1,35 @@
 /*
 Modulo de defincion de class 'Clase'
 */
+
+//previene declarar dos veces las cosas por multiples includes de este .h
+#ifndef _CLASS_CLASE_H
+#define _CLASS_CLASE_H
+
+#include <string> //http://stackoverflow.com/questions/4103169/how-do-i-include-the-string-header
+#include <iostream>
+
+/*
+como clase es la unica class que usa turno me parece al re pedo hacerle
+suu propio .h
+*/
+
+enum Turno {Manana, Tarde, Noche};
+
+/*
+defincion de la class clase
+le puse todas las cosas que estaban en el diagrama, no hay que llamar al datatype
+*/
+class Clase{
+  private:
+    int id;
+    string nombre;
+    Turno turno;
+  public:
+    virtual int cupo() = 0; //se hace asi para que sea abstracta?
+    int getId();
+    string getNombre();
+    Turno getTurno();
+};
+
+#endif
--- include/entrenamiento.h
+++ include/entrenamiento.h
@@ -1,3 +1,21 @@
 /*
 Modulo de defincion de class 'Entrenamiento'
 */
+
+//previene declarar dos veces las cosas por multiples includes de este .h
+#ifndef _CLASS_ENTRENAMIENTO_H
+#define _CLASS_ENTRENAMIENTO_H
+
+#include <iostream>
+
+class Entrenamiento{
+  private:
+    bool enRambla;
+  public:
+    DtClase(int, string, Turno);
+    int getId();
+    string getNombre();
+    Turno getTurno();
+};
+
+#endif

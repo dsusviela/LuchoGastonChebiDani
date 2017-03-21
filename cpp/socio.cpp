27b70840b98ff11c5b1857641dcb22a3ea551519
dsusviela <yothebest009@gmail.com>
2017-03-21 15:39:58 -0300

    Update socio.cpp

--- cpp/socio.cpp
+++ cpp/socio.cpp
@@ -7,6 +7,25 @@ Modulo de implementacion de class 'Socio'
 
 #include "../include/socio.h"
 
+
+Socio::Socio(){
+    socio = DtSocio();
+}
+
+Socio::Socio(DtSocio info_socio){
+    socio = info_socio;
+}
+
+DtSocio Socio::getSocio(){
+    return socio;
+}
+
+void Socio::setSocio(DtSocio info_socio){
+    socio = info_socio;
+}
+
+/*Destructora = te la debo*/
+
 /*Constructor por defecto*/
 DtSocio::DtSocio(){
   this->CI = 0;
@@ -18,19 +37,6 @@ DtSocio::DtSocio(int cedula, string name){
   this->CI = cedula;
   this->nombre = name;
 }
-
-/*Destructora, es realmente necesario esto?
-
-De lo que saco de las notas
-
-"El destructor es una operación que es invocada al momento de destruir un objeto. El
-código de un destructor se ejecuta inmediatamente antes de que el objeto sea eliminado
-de la memoria. Este código se utiliza en general para realizar alguna limpieza de
-memoria dinámica (si el objeto a destruir la utiliza)."
-
-De aca entiendo que si no hay memoria dinamica en la class no preciso definir el destructor,
-osea que su codigo esta vaico y no hay nada que hacer aca adentro*/
-DtSocio::~DtSocio(){}
 
  /*Getters*/
 int getCI::DtSocio(){

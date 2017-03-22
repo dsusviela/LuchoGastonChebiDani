35f1f1570c39b2d6fa79552be7b8f4827cb26548
dsusviela <yothebest009@gmail.com>
2017-03-22 01:26:46 -0300

    cambios principal

--- principal.cpp
+++ principal.cpp
@@ -18,25 +18,65 @@ int tope_socios = 0;
 Clase* arreglo_clases[MAX_CLASES];
 int tope_clases = 0;
 
+//funciones a implementar con sus axuliares
 static bool existe_socio(int ci) {
-    bool encontre = false;
-    int i = 0;
-    while(i < tope_socios && encontre == false) {
-        if(arreglo_socios[i]->getCI() == ci)
-            encontre = true;
-        i++;
+  bool encontre = false;
+  int i = 0;
+  while(i < tope_socios && encontre = false) {
+    if(arreglo_socios[i]->getCI() == ci){
+      encontre = true;
     }
-    return encontre;
+    i++;
+  }
+  return encontre;
 }
 
-void agregarSocio(int ci, string nombre){
-    arreglo_socios[tope_socios] = Socio(ci, nombre);
-    tope_socios++;
+void agregarSocio(int ci, string nombre){                             //1
+  arreglo_socios[tope_socios] = Socio(ci, nombre);
+  tope_socios++;
 }
 
+static bool existe_clase(DtClase &clase){
+  bool encontre = false;
+  int i = 0;
+  while(i < tope_socios && encontre = false){
+    if(arreglo_clases[i]->getId){
+      encontre = true;
+    }
+    i++;
+  }
+  return encontre;
+}
 
+void agregarClase(DtClase& clase){                                    //2
+  int id = clase.getId();
+  string nombre = clase.getNombre();
+  Turno turno = clase.getTurno();
+  Clase *ains = new Clase(id, nombre, turno);
+  arreglo_clases[tope_clases] =  ains;
+}
+
+void agregarInscripcion(String ciSocio, int idClase, Fecha fecha){    //3
+
+}
+
+void borrarInscripcion(string ciSocio, int idClase){                  //4
+
+}
+
+DtSocio **obtenerInfoSociosPorClase(id idClase, nt &cantSocios){      //5
+
+
+}
+
+DtClase obtenerClase(int idCLase){                                    //6
+
+}
+
+//-----------------------------------------------------------------------
+//CODGIO PRINCIPAL
+//-----------------------------------------------------------------------
 int main(){
-
 
 
 return 0;
